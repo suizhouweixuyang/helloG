ca44edb718e3a8a290095a3dd3a914a6e94fa5d1
suizhouiweixuyang <759236957@qq.com>
2021-11-20 11:28:38 +0800

    创建分支

--- src/main/java/com/wxy/CloudTest.java
+++ src/main/java/com/wxy/CloudTest.java
@@ -4,7 +4,5 @@ public class CloudTest {
     public static void main(String[] args) {
         System.out.println("版本一");
         System.out.println("创建分支");
-        System.out.println("分支删除");
-        System.out.println("分支测试");
     }
 }
