5b42e6c018b87d9d02b207472e045e8075f10387
suizhouweixuyang <759236957@qq.com>
2021-11-20 13:05:38 +0800

    Update CloudTest.java

--- src/main/java/com/wxy/CloudTest.java
+++ src/main/java/com/wxy/CloudTest.java
@@ -7,7 +7,7 @@ public class CloudTest {
         System.out.println("主干删除");
         System.out.println("分支删除");
         System.out.println("在线更新");
-        System.out.println("ceshi7");
+        System.out.println("ceshi9");
         
     }
 }
